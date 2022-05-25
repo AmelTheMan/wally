027aff6d48c2d2c287eddf9969e9db82bc6707f3
Amel <amelhadzic@ssklivno.net>
2022-05-25 09:26:07 +0200

    added urls for pages

---
+++ wally/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.StartingPageView.as_view(), name="starting-page"),
+    path("post/<slug:slug>", views.PostDetailView.as_view(), name="post-detail-page"),
+    path("register",views.registerPage,name="register"),
+    path("login",views.loginPage,name="login"),
+    path("logout",views.logoutUser,name="logout"),
+    
+
+    
+]
